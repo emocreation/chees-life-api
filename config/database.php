90575bda13962f1f7bbe39748ed0414265510e05
Jeff Chan <jeff@emo.hk>
2023-08-31 16:02:37 +0800

    Update timezone

--- config/database.php
+++ config/database.php
@@ -55,6 +55,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'timezone'  => '+08:00', //HKT
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
